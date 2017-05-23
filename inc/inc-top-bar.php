8cd8890e3dc26a55f435e920e3d4cc244fc1103c
Kevin Burgess <kevin_burgess@ymail.com>
2017-05-23 23:28:40 +0100

    Added the new Twittter account to the top bar links. Also made Twitter and Facebook links open in new tab/window

--- inc/inc-top-bar.php
+++ inc/inc-top-bar.php
@@ -9,15 +9,21 @@
 
 
 		<ul class="top-bar-nav">
-			<li><a href="mailto:info@foll.org.uk?Subject=Enquiry%20from%20foll.org.uk" class="tb-icons email" title="Email us">
-				<svg class="icon-top-bar icon-email"><use xlink:href="#icon-email"></use></svg>
-				</a></li>
-			<!--<li><a href="https://twitter.com/FOLLBirdNews" class="tb-icons tw" title="Twitter">
-				<svg class="icon-top-bar icon-twitter" title="Find us on Twitter"><use xlink:href="#icon-twitter"></use></svg>
-				</a></li>-->
-			<li><a href="https://www.facebook.com/foll.org.uk" class="tb-icons fb" title="Find us on Facebook">
-				<svg class="icon-top-bar icon-facebook"><use xlink:href="#icon-facebook"></use></svg>
-				</a></li>
+			<li>
+				<a href="mailto:info@foll.org.uk?Subject=Enquiry%20from%20foll.org.uk" class="tb-icons email" title="Email us">
+					<svg class="icon-top-bar icon-email"><use xlink:href="#icon-email"></use></svg>
+				</a>
+			</li>
+			<li>
+				<a href="https://twitter.com/FOLLLatestNews" target="_blank" class="tb-icons tw" title="Twitter">
+					<svg class="icon-top-bar icon-twitter" title="Find us on Twitter"><use xlink:href="#icon-twitter"></use></svg>
+				</a>
+			</li>
+			<li>
+				<a href="https://www.facebook.com/foll.org.uk" target="_blank" class="tb-icons fb" title="Find us on Facebook">
+					<svg class="icon-top-bar icon-facebook"><use xlink:href="#icon-facebook"></use></svg>
+				</a>
+			</li>
 
 			<li class="search-btn">
 				<div class="widget_search"><?php get_search_form(); ?></div>
