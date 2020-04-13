585e5cfea9fc432d805cd3e960cc275bcad240c5
Jason Righelato <jayontraps@gmail.com>
2020-04-13 09:17:51 +0100

    headings

--- page-home.php
+++ page-home.php
@@ -61,7 +61,7 @@ get_header(); ?>
 			</div>
 
 			<div class="h-panel">							
-				<h2><a href="/">Latest News & Events</a></h2>						
+				<h2><a href="/">Latest News</a></h2>						
 				<div class="h-panel__inner">
 					<?php include('inc/feed-events.php') ?>			    
 				</div>
